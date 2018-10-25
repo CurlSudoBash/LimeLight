50eaf60fafc68b5abcb68093908a18b8f6d44878
Nikhil Kaushik <nikhil.celtics@gmail.com>
2018-10-25 23:15:18 +0530

    adds map activity

--- app/src/main/java/com/example/kriti/uiassign/MainActivity.java
+++ app/src/main/java/com/example/kriti/uiassign/MainActivity.java
@@ -4,14 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button victimBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        victimBtn = (Button) findViewById(R.id.victim_button);
+        victimBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 
     public void toastMsg(String msg) {
