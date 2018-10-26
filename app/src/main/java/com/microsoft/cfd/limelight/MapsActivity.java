e141e00491706bb3d59063fcf44e2c5f737643b2
Alphadose <anish.mukherjee1996@gmail.com>
2018-10-26 17:46:04 +0530

    Maps in Working Condition

--- app/src/main/java/com/microsoft/cfd/limelight/MapsActivity.java
+++ app/src/main/java/com/microsoft/cfd/limelight/MapsActivity.java
@@ -50,7 +50,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        //mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         mClusterManager = new ClusterManager<MyItem>(this, mMap);
         mMap.setOnCameraIdleListener(mClusterManager);
         mMap.setOnMarkerClickListener(mClusterManager);
